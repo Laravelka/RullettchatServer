f48ec662ef435268d67a7c8598ea32ae5e8c0c2d
Laravelka <laravel.coder@yandex.ru>
2021-06-16 09:26:55 +0300

    Некоторые обновы

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    db: {
-        host : 'localhost',
-        user : 'root',
-        password : 'Qazxsw102',
-        database : 'rullettchatt',
-        charset: 'utf8mb4_unicode_ci'
-    }
-};
--- index.js
+++ index.js
@@ -1,31 +1,62 @@
 require('dotenv').config();
 
-const sha = require('sha256');
-const User = require("./Models/User.js");
+const app = require('express')();
+const http = require('http').createServer(app);
+const cors = require('cors');
+const sha256 = require('sha256');
+const UserModel = require("./Models/User.js");
 
-// const express = require("express");
-// const app = express();
-
-// const server = require("http").Server(app);
-/*
-app.get("/", (req, res) => {
-	res.status(200).send("socket.io server");
-});
-*/
 async function getUser() {
-	const user = await (new User).find(1);
+	const user = await (new UserModel).find(1);
 
 	console.log(user);
 }
 
-getUser();
+const PORT = process.env.PORT || 5000;
 
-const httpServer = require("http").createServer();
-const options = { /* ... */ };
-const io = require("socket.io")(httpServer, options);
+app.use(cors());
+app.options('*', cors());
 
-io.on("connection", (socket) => {
-	console.log(socket);
+const io = require('socket.io')(http, {
+	cors: {
+		origin: "*",
+		methods: ["GET", "POST"]
+	}
 });
 
-httpServer.listen(3000);
+const signalServer = require('simple-signal-server')(io);
+const allIDs = new Set();
+
+signalServer.on('discover', (request) => {
+	const clientID = request.socket.id; // clients are uniquely identified by socket.id
+
+	allIDs.add(clientID); // keep track of all connected peers
+
+	request.discover(Array.from(allIDs)); // respond with id and list of other peers
+
+	console.log('discovered:', clientID);
+});
+
+signalServer.on('disconnect', (socket) => {
+	const clientID = socket.id;
+
+	allIDs.delete(clientID);
+
+	console.log('disconnected:', clientID);
+});
+
+signalServer.on('request', (request) => {
+	request.forward(); // forward all requests to connect
+
+	console.log('request: ', request.initiator, 'to', request.target);
+});
+
+app.get('/', (req, res) => {
+	res.send('Сервер успешно запущен');
+
+	console.log('home');
+});
+
+http.listen(PORT, () => {
+	console.log(`Listening to ${PORT}`);
+});
